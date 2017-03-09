c66a46a0f58728bf450c6b628d22326e50a5d52d
Roman <kaboom.ua@gmail.com>
2017-03-09 11:36:04 +0200

    Update accordeon.js

--- src/decorators/accordeon.js
+++ src/decorators/accordeon.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//DecoratedComponent я называл чтоб легче понять было. Лучше выбирай более значущее название
 export default (CustomComponent) => class DecoratedComponent extends React.Component {
   state = {
       openEntityId: null
