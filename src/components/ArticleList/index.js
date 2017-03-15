963023ccdbb0ddca2da76e4ba0df727e5181c4e7
Dmitry Sadovsky <dmitry.sadovskiy@insales.ru>
2017-03-15 18:49:46 +0300

    Реализован фильтр для статей

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -9,7 +9,9 @@ class ArticleList extends Component {
     render() {
         const {articles, toggleOpenItem, isItemOpened} = this.props
 
-        const articleComponents = articles.map(article => <li key={article.id}>
+        const articleComponents = articles
+            .filter(this.filterArticles)
+            .map(article => <li key={article.id}>
             <Article article={article}
                      isOpen={isItemOpened(article.id)}
                      toggleOpen={toggleOpenItem(article.id)}
@@ -28,12 +30,19 @@ class ArticleList extends Component {
             </CSSTransition>
         )
     }
+
+    filterArticles = article => {
+        const date_ms = new Date(article.date).getTime()
+        const {from, to} = this.props.filter
+        return (!from || from.getTime() <= date_ms) && (!to || date_ms <= to.getTime())
+    }
 }
 
 const mapStateToProps = state => {
     console.log('---', 'connect, state = ', state)
     return {
-        articles: state.articles
+        articles: state.articles,
+        filter: state.filter
     }
 }
 
